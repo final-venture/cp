0b35069786c5e7adf5dd47e6bdca3d63de5ede00
Cyril Chen <2385658016cc@gmail.com>
2024-01-06 18:37:31 +1300

    .

---
+++ LeetCode/853.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        cars = [[p, s] for p, s in zip(position, speed)]
+
+        cars.sort(reverse=True)
+
+        for p, s in cars:
+            stack.append((target-p)/s)
+            if len(stack) >= 2:
+                if (target-p)/s <= stack[-2]:
+                    stack.pop()
+        
+        return(len(stack))
