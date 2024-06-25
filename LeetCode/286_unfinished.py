fecee5eaf00f8a05c981e2bd44072dedda931781
Cyril Chen <2385658016cc@gmail.com>
2024-06-25 15:13:51 +1200

    .

--- LeetCode/286_unfinished.py
+++ LeetCode/286_unfinished.py
@@ -20,4 +20,26 @@ class Solution:
                         continue
                     grid[nr][nc] = count + 1
                     q.append([nr, nc, count+1])
+
+# Optimal from HTH sajj
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        inf = 2147483647
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        q = deque()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    q.append((r, c))
+
+        while q:
+            cr, cc = q.popleft()
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for dr, dc in directions:
+                nr, nc = cr + dr, cc + dc
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == inf:
+                    grid[nr][nc] = grid[cr][cc] + 1
+                    q.append((nr, nc))
         
