05abc52162b492bdd8d3385ce5774fed1feeb8ca
Cyril Chen <2385658016cc@gmail.com>
2024-11-19 11:15:34 +1300

    .

--- codeforces/test/template.cpp
+++ codeforces/test/template.cpp
@@ -18,15 +18,63 @@ void init()
     cin.tie(0);
 }
 
+int n;
+vector<pair<int, int>> seg;
+vector<int> nums;
+
+void build(int idx, int l, int r)
+{
+    if (l == r)
+    {
+        seg[idx] = {nums[l], nums[l]};
+        return;
+    }
+    int mid = (r + l) / 2;
+    build(idx * 2 + 1, l, mid);
+    build(idx * 2 + 2, mid + 1, r);
+    seg[idx].first = seg[idx * 2 + 1].first + seg[idx * 2 + 2].first;
+    seg[idx].second = max({seg[idx].first,
+                           seg[idx * 2 + 1].second, seg[idx * 2 + 2].second
+                          });
+}
+
+int query(int idx, int l, int r, int ql, int qr)
+{
+    if (l > qr || r < ql) return INT_MIN;    // outside
+    if (l >= ql && r <= qr) return seg[idx].second; // l, r inside query
+    // overlap:
+    int mid = (r + l) / 2;
+    int left = query(idx * 2 + 1, l, mid, ql, qr);
+    int right = query(idx * 2 + 2, mid + 1, r, ql, qr);
+    return max(left, right);
+}
+
 void solve()
 {
+    cin >> n;
+    nums = vector<int>(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    seg = vector<pair<int, int>>(4 * n);
+    build(0, 0, n - 1);
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        int ql, qr;
+        cin >> ql >> qr;
+        --ql; --qr;
+        int res = query(0, 0, n - 1, ql, qr);
+        cout << res << '\n';
+    }
 }
 
 int main()
 {
     init();
-    int t;
-    cin >> t;
+    int t = 1;
     while (t--)
         solve();
     return 0;
