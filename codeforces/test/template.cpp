2cd3513be99cff89f6e176009977a196fe7b4cd7
Cyril Chen <2385658016cc@gmail.com>
2024-11-20 15:13:33 +1300

    .

--- codeforces/test/template.cpp
+++ codeforces/test/template.cpp
@@ -18,17 +18,76 @@ void init()
     cin.tie(0);
 }
 
-void solve()
+int city = -1;
+
+bool solve()
 {
+    ++city;
+    int n;
+    int qst = 0;
+    if (!(cin >> n)) return false;
+    unordered_map<int, vector<int>> adjl{};
+    for (int i = 0; i < n; ++i)
+    {
+        int x, y;
+        cin >> x >> y;
+        qst = max({qst, x, y});
+        adjl[x].pb(y);
+    }
+    vector<vector<int>> adj(qst + 1, vector<int>(qst + 1, 0));
+    for (auto& [from, neis] : adjl)
+    {
+        for (int nei : neis)
+        {
+            ++adj[from][nei];
+        }
+    }
+    for (int via = 1; via < qst + 1; ++via)
+    {
+        for (int i = 0; i < qst + 1; ++i)
+        {
+            for (int j = 0; j < qst + 1; ++j)
+            {
+                adj[i][j] = adj[i][j] + adj[i][via] * adj[via][j];
+            }
+        }
+    }
+
+    for (int via = 1; via < qst + 1; ++via)
+    {
+        for (int i = 0; i < qst + 1; ++i)
+        {
+            for (int j = 0; j < qst + 1; ++j)
+            {
+                adj[i][j] = adj[i][j] + adj[i][via] * adj[via][j];
+            }
+        }
+    }
+
+    for (int x = 0; x < qst + 1; ++x)
+    {
+        if (adj[x][x])
+        {
+
+        }
+    }
+
+    cout << "matrix for city " << city << '\n';
+    for (int i = 0; i < qst + 1; ++i)
+    {
+        for (int j = 0; j < qst + 1; ++j)
+        {
+            cout << adj[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return true;
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
+    init();
+    while (solve());
     return 0;
 }
