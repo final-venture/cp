8141b6b8b5b412ac48524b3dc12d20193520a6b4
Cyril Chen <2385658016cc@gmail.com>
2024-11-30 23:38:43 +1300

    .

--- codeforces/test/template.cpp
+++ codeforces/test/template.cpp
@@ -21,6 +21,42 @@ void init()
 
 void solve()
 {
+    int n;
+    cin >> n;
+    int a[n];
+    int res = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    if (n == 1)
+    {
+        cout << 1 + a[0] << '\n';
+        return;
+    }
+    vector<pair<int, int>> dp(n, {0, 0});
+    dp[n - 1] = {1, a[n - 1]};
+    dp[n - 2] = {1, a[n - 2]};
+    for (int i = n - 3; i >= 0; --i)
+    {
+        for (int j = i + 2; j < n; ++j)
+        {
+            if (dp[j].first + 1 + dp[j].second > dp[j].first + 1 + a[i])
+            {
+                dp[i] = {dp[j].first + 1, dp[j].second};
+            }
+            else
+            {
+                dp[i] = {dp[j].first + 1, a[i]};
+            }
+        }
+    }
+    for (auto& [x, y] : dp) cout << x << ' ' << y << '\n';
+    for (int i = 0; i < n; ++i)
+    {
+        res = max(res, dp[i].first + dp[i].second);
+    }
+    cout << res << '\n';
 }
 
 signed main()
