719e35573a1bfd27a9f3c2f5cb2c4533be649f9b
Cyril Chen <2385658016cc@gmail.com>
2024-12-18 21:22:34 +1300

    .

--- !test/codeforces/template.cpp
+++ !test/codeforces/template.cpp
@@ -21,66 +21,34 @@ void init()
 
 int n, m;
 vector<vector<int>> adj;
-vector<int> col;
 
-bool dfs(int node, int c)
+bool dfs(int node)
 {
-    col[node] = c;
     for (int nei : adj[node])
     {
-        if (col[nei] == -1)
-        {
-            if (!dfs(nei, c ^ 3))
-                return false;
-        }
-        else if (col[nei] == col[node])
-        {
-            return false;
-        }
+        dfs(nei);
     }
-    return true;
 }
 
 void solve()
 {
     cin >> n >> m;
     adj.resize(n + 1);
-    col.resize(n + 1, -1);
     for (int i = 0; i < m; ++i)
     {
-        int u, v;
-        cin >> u >> v;
-        adj[u].pb(v);
-        adj[v].pb(u);
-    }
-    int valid = 1;
-    for (int i = 1; i < n + 1; ++i)
-    {
-        if (col[i] == -1 && !dfs(i, 1))
-        {
-            valid = 0;
-            break;
-        }
-    }
-    if (!valid)
-    {
-        cout << "IMPOSSIBLE" << '\n';
-    }
-    else
-    {
-        for (int i = 1; i < n + 1; ++i)
-        {
-            cout << col[i] << ' ';
-        }
+        int a, b;
+        cin >> a >> b;
+        adj[a].pb(b);
+        adj[b].pb(a);
     }
 }
 
 signed main()
 {
     init();
-    // int t;
-    // cin >> t;
-    // while (t--)
-    solve();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
