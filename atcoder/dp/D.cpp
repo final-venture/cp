584cf7ddf37e769d67211b275e16e09937775743
Cyril Chen <2385658016cc@gmail.com>
2025-01-04 06:58:06 +1300

    .

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -13,6 +13,7 @@ typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics
 
 #define all(x) (x).begin(), (x).end()
 #define EPS 1e-9
+#define int ll
 
 void init()
 {
@@ -24,28 +25,32 @@ void init()
     cin.tie(0);
 }
 
-const int maxx = 250000005;
-
 void solve()
 {
-    int n, k;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i)
+    int n, w;
+    cin >> n >> w;
+    vector<int> wts(n + 1);
+    vector<int> vals(n + 1);
+    for (int i = 1; i <= n; ++i)
     {
-        cin >> a[i];
+        cin >> wts[i] >> vals[i];
     }
-
-    vector < bitset
-
-    int k;
-    cin >> k;
-    for (int i = 0; i < k; ++i)
+    vector<vector<int>> dp(n + 1, vector<int>(w + 1, 0));
+    for (int i = 1; i <= n; ++i)
     {
-        int q;
-        cin >> q;
-
+        for (int j = 1; j <= w; ++j)
+        {
+            if (j - wts[i] >= 0)
+                dp[i][j] = dp[i - 1][j - wts[i]] + vals[i];
+            dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+        }
     }
+    ll ret = 0;
+    for (int j = 0; j <= w; ++j)
+    {
+        ret = max(ret, dp[n][j]);
+    }
+    cout << ret << '\n';
 }
 
 signed main()
