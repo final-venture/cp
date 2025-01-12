7e3ae22b93e04ec825866bdc078747fdf9afc2bf
Cyril Chen <2385658016cc@gmail.com>
2025-01-13 10:10:13 +1300

    .

---
+++ !templates/cses.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define EPS 1e-9
+
+void solve()
+{
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
---
+++ !test/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define EPS 1e-9
+
+void solve()
+{
+    int x;
+    cin >> x;
+    cout << x;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
