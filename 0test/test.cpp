d35d106f6131286be7c2e89a07d6b9a69a9cca42
Cyril Chen <2385658016cc@gmail.com>
2025-01-24 10:41:57 +1300

    .

---
+++ 0test/test.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define EPS 1e-9
+
+void solve()
+{
+    int l, r;
+    cin >> l >> r;
+    cout << r - l << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
