19357ecf4119739d1834e9f33ccdae30ce971aff
Cyril Chen <2385658016cc@gmail.com>
2025-07-08 20:11:09 +1200

    .

--- leetcode/1751.py
+++ leetcode/1751.py
@@ -14,3 +14,21 @@ class Solution:
             nxt = bisect_right(events, [events[idx][1], inf, inf])
             return max(dfs(nxt, remaining - 1) + events[idx][2], dfs(idx + 1, remaining))
         return dfs(0, k)
+
+# Cached Binary Search Solution O(n(k + log n))
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort()
+        n = len(events)
+        next_event = [n] * n
+        for i in range(n):
+            ed = events[i][1]
+            next_event[i] = bisect_right(events, [ed, inf, inf])
+
+        @cache
+        def dfs(idx, remaining):
+            if idx >= n or not remaining:
+                return 0
+            return max(dfs(next_event[idx], remaining - 1) + events[idx][2], dfs(idx + 1, remaining))
+        return dfs(0, k)
+
