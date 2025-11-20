0483e16544ee23fdb572d92ef332d1ff32221164
Cyril Chen <cyrilchen31@gmail.com>
2025-11-20 17:51:23 +1300

    .

--- leetcode/3748.cpp
+++ leetcode/3748.cpp
@@ -29,40 +29,50 @@ public:
         // if (starts.size() != ends.size()) ends.push_back(nn - 1);
 
         int sn = starts.size();
-        vector<int> sscnts(sn);
-        vector<int> sssfx(sn, 0);
-        vector<int> sspfx(sn, 0);
+        vector<long long> sscnts(sn);
+        vector<long long> sspfx(sn, 0);
+        // vector<long long> sssfx(sn, 0);
 
         for (int i = 0; i < sn; ++i)
         {
-            int L = ends[i] - starts[i] + 1;
+            long long L = ends[i] - starts[i] + 1;
             sscnts[i] = L * (L + 1) / 2;
         }
-        for (int i = sn - 2; i >= 0; --i)
-        {
-            sssfx[i] = sssfx[i + 1] + sscnts[i + 1];
-        }
+
+        // for (int i = sn - 2; i >= 0; --i) {
+        //     sssfx[i] = sssfx[i + 1] + sscnts[i + 1];
+        // }
+
         for (int i = 1; i < sn; ++i)
         {
             sspfx[i] = sspfx[i - 1] + sscnts[i - 1];
         }
 
-        debug(sn, starts);
-        debug(sn, ends);
-        debug(sn, sspfx);
-        debug(sn, sssfx);
+        // debug(sn, starts);
+        // debug(sn, ends);
+        // debug(sn, sscnts);
+        // debug(sn, sspfx);
+        // debug(sn, sssfx);
 
-        vector<long long> rets(qn, sssfx[0] + sscnts[0]);
+        vector<long long> rets(qn, 0);
         for (int i = 0; i < qn; ++i)
         {
             auto &q = queries[i];
             int st = q[0], ed = q[1];
-            int startsIdx = lower_bound(starts.begin(), starts.end(), st) - starts.begin();
-            int endsIdx = lower_bound(ends.begin(), ends.end(), ed) - ends.begin();
-            rets[i] -= sspfx[startsIdx];
-            rets[i] -= sssfx[endsIdx];
-            rets[i] -= (st - starts[startsIdx]);
-            rets[i] -= (ends[endsIdx] - ed);
+            int endsIdx1 = lower_bound(ends.begin(), ends.end(), st) - ends.begin();
+            int endsIdx2 = lower_bound(ends.begin(), ends.end(), ed) - ends.begin();
+            if (endsIdx1 == endsIdx2)
+            {
+                long long L = ed - st + 1;
+                rets[i] += L * (L + 1) / 2;
+            }
+            else
+            {
+                rets[i] += sspfx[endsIdx2] - sspfx[endsIdx1] - sscnts[endsIdx1];
+                long long L1 = ends[endsIdx1] - st + 1, L2 = ed - starts[endsIdx2] + 1;
+                rets[i] += L1 * (L1 + 1) / 2;
+                rets[i] += L2 * (L2 + 1) / 2;
+            }
         }
 
         return rets;
