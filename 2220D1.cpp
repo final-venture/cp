c85838aa8321b456b654eb1b75db302f931d3254
Cyril Chen <2385658016cc@gmail.com>
2026-05-07 20:01:43 +1000

    .

---
+++ 2220D1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define EPS 1e-9
+
+void solve()
+{
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
